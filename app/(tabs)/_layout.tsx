d1eda487a948b6bdc8232f1c69c27a578cd9ec50
oleksandr.petrenko <tarakanillius@proton.me>
2025-12-09 15:53:25 +0100

    Initial commit

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,25 @@
+import {NativeTabs, Icon, Label, Badge} from 'expo-router/unstable-native-tabs';
+
+export default function TabLayout() {
+    return (
+        <NativeTabs>
+            <NativeTabs.Trigger name="contacts">
+                <Label>Contacts</Label>
+                <Icon sf="person.circle.fill" drawable="custom_contacts_drawable" />
+            </NativeTabs.Trigger>
+            <NativeTabs.Trigger name="calls">
+                <Label>Calls</Label>
+                <Icon sf="phone.fill" drawable="custom_phone_drawable" />
+            </NativeTabs.Trigger>
+            <NativeTabs.Trigger name="index">
+                <Label>Chats</Label>
+                <Badge>326+</Badge>
+                <Icon sf="message.fill" drawable="custom_message_drawable" />
+            </NativeTabs.Trigger>
+            <NativeTabs.Trigger name="settings">
+                <Label>Settings</Label>
+                <Icon sf="gear" drawable="custom_settings_drawable" />
+            </NativeTabs.Trigger>
+        </NativeTabs>
+    );
+}
